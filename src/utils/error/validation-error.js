f1084e749c81688cd3260abfbad1cf778e21c649
arvindpndit <arvindpandi9808t@gmail.com>
2023-08-31 18:52:55 +0530

    error classes added

---
+++ src/utils/error/validation-error.js
@@ -0,0 +1,17 @@
+const {StatusCodes} = require("http-status-codes");
+
+class ValidationError extends Error {
+    constructor(){
+        super();
+        let explanation = [];
+        error.errors.forEach(error => {
+            explanation.push(error.message);
+        });
+        this.name = 'ValidationError';
+        this.message = 'Not able to validate the data sent in the request';
+        this.explanation = explanation;
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+module.exports = ValidationError;
