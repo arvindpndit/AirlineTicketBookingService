9610af911f0f925d9a908081b9735a516a7e9081
arvindpndit <arvindpandit9808@gmail.com>
2023-08-31 19:46:15 +0530

    ignore this commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const {PORT} = require("./config/serverConfig")
-const apiRoutes = require('./routes/index');
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
 
 const app = express();
 
 const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+  app.use("/api", apiRoutes);
 
-    app.use("/api", apiRoutes);
+  app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`);
+  });
+};
 
-    app.listen(PORT, ()=>{
-        console.log(`Server is up and running on PORT ${PORT}`)
-    })
-}
+setupAndStartServer();
 
-setupAndStartServer()
+//ignore this line
