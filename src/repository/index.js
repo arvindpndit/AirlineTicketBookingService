8f626669ee7f5e9a86f3b020ec64357929a90719
arvindpndit <arvindpandit9808@gmail.com>
2023-09-02 09:48:05 +0530

    added repository and basic service file

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,22 +1,3 @@
-const { Booking } = require("../models/index)");
-
-class BookingRepository {
-  async create(data) {
-    try {
-      const booking = await Booking.create(data);
-      return booking;
-    } catch (error) {
-      if (error.name == "SequelizeValidationError") {
-        throw new ValidationError(error);
-      }
-      throw new AppError(
-        "RepositoryError",
-        "Cannot create Booking",
-        "There was some issue creating the booking, please try again later",
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
-    }
-  }
-}
-
-module.exports = BookingRepository;
+module.exports = {
+  BookingRepository: require("./booking-repository"),
+};
